3d1bc33bcc61fa036902d969fdacc9d7dc379372
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-21 22:54:24 +0000

    added welcome function
    this is a function that prints out a full name of a user

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,15 @@
+/*
+* welcome - prints out the full name of a user
+* @firstName: First name of the user (String)
+* @lastName: Last name of the user (String)
+* Return: does not return any values
+*/
+function welcome (firstName, lastName) {
+    let fullName = (firstName  + ' ' + lastName);
+
+    function displayFullName () {
+        alert("Welcome " + fullName + " !");
+    }
+
+    displayFullName();
+}
