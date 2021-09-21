348978bfe038c1cb8a7dda21c25b6ba2c1690b96
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-21 22:56:45 +0000

    removed space from exclamation mark on line 11

--- 0x0C-Javascript_advanced/0-welcome.js
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -8,7 +8,7 @@ function welcome (firstName, lastName) {
     let fullName = (firstName  + ' ' + lastName);
 
     function displayFullName () {
-        alert("Welcome " + fullName + " !");
+        alert("Welcome " + fullName + "!");
     }
 
     displayFullName();
