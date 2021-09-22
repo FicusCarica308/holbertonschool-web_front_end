bff526c428492e0f4e2ee9fcf167d4888fe18b81
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-22 00:01:20 +0000

    added task 3

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,15 @@
+
+function createClassRoom (numbersOfStudents) {
+    function studentSeat (seat) {
+        return function() {
+            return seat + 1;
+        }
+    }
+    let students = new Array(numbersOfStudents); 
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students[i] = studentSeat(i);
+    }
+    return (students);
+}
+
+let classRoom = createClassRoom(10);
