c874bf775fa11f7f797acd25758bae08a858511d
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-23 05:39:45 +0000

    added task 9

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,22 @@
+function countPrimeNumbers () {
+    let is_prime = true;
+    let numCount = 0;
+    for (let i = 2; i <= 100; i++) {
+        is_prime = true;
+        for(let k = 2; k <= i - 1; k++) {
+            if (i % k == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime == true)
+            numCount++;
+    }
+    return (numCount);
+}
+
+const timeStart = performance.now();
+countPrimeNumbers();  
+const timeFinish = performance.now();
+const finalTime = timeFinish - timeStart;
+console.log("Execution time of printing countPrimeNumbers was " + finalTime + " milliseconds.");
